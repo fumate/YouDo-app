d731af1006cb72b3c8d42d571086d44becead22a
fumate <mate.fuller@gmail.com>
2021-02-06 11:39:16 +0100

    added mz-04

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,11 @@
 let youDoform = document.getElementById("youDoForm");
 
-
+youDoform.addBtn.disabled = true;
 
 document.addEventListener("DOMContentLoaded", () => {
 
     let addBtn = document.getElementById("addBtn");
     let youDoList = document.getElementById("youDoList");
-
 
 
 
@@ -26,14 +25,23 @@ document.addEventListener("DOMContentLoaded", () => {
         removeBtn.addEventListener("click", event => {
             liEl.remove();
             removeBtn.remove();
-        })
-
-
-
+        });
 
 
 
         console.log("Console log works!");
+    });
+
+    youDoform.textForTodos.addEventListener("keyup", event => {
+                if (event.target.value.length > 6) {
+                    event.target.classList.add("success");
+                    event.target.classList.remove("error");
+                    youDoform.addBtn.disabled = false;
+                } else {
+                    event.target.classList.add("error");
+                    event.target.classList.remove("success");
+                    youDoform.addBtn.disabled = true;
+                }
     });
 });
 
