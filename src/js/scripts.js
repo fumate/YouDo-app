1058941a9c2dbe332b86894ca00c353b328bc551
fumate <mate.fuller@gmail.com>
2021-02-06 16:52:37 +0100

    added mz-05

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,12 @@
 let youDoform = document.getElementById("youDoForm");
 
+let selectPriors = document.getElementById("selecPriors");
+
+let selectField = document.getElementById("selectField");
+
+
 youDoform.addBtn.disabled = true;
+
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -8,28 +14,27 @@ document.addEventListener("DOMContentLoaded", () => {
     let youDoList = document.getElementById("youDoList");
 
 
-
     addBtn.addEventListener("click", event => {
         event.preventDefault();
         let youDoText = youDoform.textForTodos.value;
         let removeBtn = document.createElement("button");
         let liEl = document.createElement("li");
+        youDoform.textForTodos.value = "";
 
-        removeBtn.innerText = "Remove";
 
         liEl.textContent = youDoText;
+        removeBtn.innerText = "Remove";
 
         youDoList.prepend(removeBtn);
         youDoList.prepend(liEl);
+        youDoList.prepend(selectPriors);
 
         removeBtn.addEventListener("click", event => {
             liEl.remove();
             removeBtn.remove();
+            selectPriors.remove();
         });
 
-
-
-        console.log("Console log works!");
     });
 
     youDoform.textForTodos.addEventListener("keyup", event => {
@@ -37,22 +42,36 @@ document.addEventListener("DOMContentLoaded", () => {
                     event.target.classList.add("success");
                     event.target.classList.remove("error");
                     youDoform.addBtn.disabled = false;
-                } else {
+                } else if (event.target.value === "") {
+                    event.target.classList.remove("success");
+                }
+                else {
                     event.target.classList.add("error");
                     event.target.classList.remove("success");
                     youDoform.addBtn.disabled = true;
                 }
     });
+
 });
 
+/*let todos = [];
 
+function addTodo() {
+    let inputValue = document.getElementById(youDoform.textForTodos.value);
+    todos.push(inputValue);
+}
+function printTodos() {
+    let list = document.getElementById("youDoList");
+    list.innerHTML = "";
 
-
-
-
-
-
-
-
-
-//console.log("Console log works!");
+    for (let i = 0; i < todos.length; i++) {
+        let i = document.createElement("li");
+        let listItem = li.appendChild((document.createTextNode(todos[i])));
+        list.appendChild(listItem);
+        console.log(todos);
+    }
+    document.getElementById("click", function() {
+        addTodo();
+        printTodos();
+    })
+}*/
