fcc0e5c91c81dcc81ab30225c972f76aef957ca8
fumate <mate.fuller@gmail.com>
2021-02-06 10:37:32 +0100

    added mz-01

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,40 @@
-//can be deleted
-console.log("Console log works!");
+let youDoform = document.getElementById("youDoForm");
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    let addBtn = document.getElementById("addBtn");
+    let youDoList = document.getElementById("youDoList");
+
+
+
+
+    addBtn.addEventListener("click", event => {
+        event.preventDefault();
+        let youDoText = youDoform.textForTodos.value;
+        let liEl = document.createElement("li");
+
+        liEl.textContent = youDoText;
+
+        youDoList.prepend(liEl);
+
+
+
+
+
+        console.log("Console log works!");
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+//console.log("Console log works!");
